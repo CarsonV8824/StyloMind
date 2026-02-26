6d27fce4947241eb7944b12f474c7e60215b339f
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2026-02-26 17:08:26 -0600

    fixed bug

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,14 +1,25 @@
 # ui/main_window.py
-from PySide6.QtWidgets import QMainWindow, QStackedWidget
+from PySide6.QtWidgets import QMainWindow, QStackedWidget, QHBoxLayout, QWidget
 from ui.widgets.button_container import ButtonContainer
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.stack = QStackedWidget()
-        self.setCentralWidget(self.stack)
-
+        self.main_widget = QWidget()
+        self.main_layout = QHBoxLayout()
+        self.setCentralWidget(self.main_widget)
+        self.main_widget.setLayout(self.main_layout)
         self.buttons = ButtonContainer()
+        self.stack = QStackedWidget()
+        self.main_layout.addWidget(self.buttons)
+        self.main_layout.addWidget(self.stack)
         self.buttons.go_to_page.connect(self.stack.setCurrentIndex)
+       
+        
+        
+    
+        
+
+
 
         # add pages to stack...
